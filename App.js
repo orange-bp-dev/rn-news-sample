b62c5f32c15b560b8d09d2beffc45a4aaca5264e
Sato Ryoka <satoryoka0920@gmail.com>
2021-07-29 15:37:03 +0900

    api fetch

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { StyleSheet, View, FlatList, SafeAreaView } from "react-native"
 import { ListItem } from "./components/ListItem"
-import articles from "./dummies/articles.json"
+// import articles from "./dummies/articles.json"
+import Constants from "expo-constants"
+import axios from "axios"
 
 const styles = StyleSheet.create({
   container: {
@@ -11,7 +13,27 @@ const styles = StyleSheet.create({
   }
 })
 
+const apiKey = Constants.manifest.extra.newsApiKey
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${apiKey}`
+
 export default function App() {
+  const [articles, setArticles] = useState([])
+
+  const fetchArticles = async () => {
+    try {
+      const res = await axios.get(URL)
+      console.log(res)
+      setArticles(res.data.articles)
+    } catch (error) {
+      console.error("error-------------------------", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchArticles()
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       {/* {articles.map((article, idx) => {
